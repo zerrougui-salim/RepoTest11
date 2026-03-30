f444eedfed12489af90dc73e89260a6bcd558b3e
zerrouguisalim-dotcom <zerrougui.salim@gmail.com>
2026-03-30 20:06:06 +0100

    commit to test pull request

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-print("Hello, World! 2")
+print("Hello,")
 print("Welcome to the world of programming!")
 
 # Code from new feature 1 branch
 
 # commit in the same last one  2   
+
+
+# example of a pull request 
