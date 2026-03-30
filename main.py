2d07847121a8ce32ccdcc2cb74f63df2587c0731
zerrougui salim <salim.zerrougui@umc.edu.dz>
2026-03-30 19:12:57 +0100

    LAST COMMIT

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, World!")
+print("Hello, World! 2")
 print("Welcome to the world of programming!")
 
 # Code from new feature 1 branch
 
-# commit in the same last one  2
+# commit in the same last one  2   
